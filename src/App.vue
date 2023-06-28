3bc962f5529cfc76f5ddc4c14a3f89565c3dead0
caoliang8 <122992803+caoliang8@users.noreply.github.com>
2023-06-28 17:13:11 +0800

    Add files via upload
    通过拖拽文件，实现上传

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tale-box">
+    <div class="title">CRUD Demo</div>
+ 
+  <div class="query-box">
+    <el-input v-model="queryInput" placeholder="Please input" />
+    <el-button type="primary">Primary</el-button>
+  </div>
+ </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+let queryInput=ref('')
+</script>
+
+
+<style scoped>
+.title{
+  text-align: center;
+}
+</style>
